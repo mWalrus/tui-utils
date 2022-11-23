c87a7c3937898fc3282d6faffd0ea31646a839a5
mWalrus <hellkvistoskar@protonmail.com>
2022-11-23 21:59:04 +0100

    feat: terminal handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use crossterm::event::{self, Event};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::error::Error;
+use std::io;
+use std::time::Duration;
+use tui::backend::{Backend, CrosstermBackend};
+use tui::{Frame, Terminal};
+
+pub(crate) static EVENT_TIMEOUT: Duration = Duration::from_millis(1000);
+pub type TerminalResult<T> = std::result::Result<T, Box<dyn Error>>;
+
+/// Prepare the terminal by flushing the screen and hiding the cursor
+pub fn init_terminal() -> TerminalResult<Terminal<CrosstermBackend<io::Stdout>>> {
+    crossterm::execute!(io::stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+    let backend = CrosstermBackend::new(io::stdout());
+
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    Ok(terminal)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Restore the terminal to its initial state
+pub fn restore_terminal() -> TerminalResult<()> {
+    disable_raw_mode()?;
+    crossterm::execute!(io::stdout(), LeaveAlternateScreen)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    Ok(())
+}
+
+/// Poll for crossterm events such as key events
+pub fn poll_event() -> TerminalResult<Option<Event>> {
+    if event::poll(EVENT_TIMEOUT)? {
+        Ok(Some(event::read()?))
+    } else {
+        Ok(None)
     }
 }
