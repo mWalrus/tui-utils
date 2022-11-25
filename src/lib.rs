762e2d3de18452c28bf1145175cbd4a3521bdb31
mWalrus <hellkvistoskar@protonmail.com>
2022-11-25 23:49:05 +0100

    feat: adding rect helper

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 pub mod blocks;
 pub mod keymap;
+pub mod rect;
 pub mod term;
 
 pub(crate) static EVENT_TIMEOUT: Duration = Duration::from_millis(1000);
---
+++ src/rect.rs
@@ -0,0 +1,16 @@
+use tui::layout::Rect;
+
+/// Produces a centered rect half the size of the width as well as the height
+pub fn centered_rect(size: Rect) -> Rect {
+    let width = size.width / 2;
+    let x = width / 2;
+    let height = size.height / 2;
+    let y = height / 2;
+
+    Rect {
+        x,
+        y,
+        width,
+        height,
+    }
+}
