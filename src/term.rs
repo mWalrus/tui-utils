1603518fe39eca86451ff70bc335fa8a1540d125
mWalrus <hellkvistoskar@protonmail.com>
2022-11-26 19:08:35 +0100

    feat: improving error handling in term module

--- src/term.rs
+++ src/term.rs
@@ -3,30 +3,42 @@ use crossterm::event::{self, Event};
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
-use std::error::Error;
 use std::io;
+use thiserror::Error;
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
-pub type TerminalResult<T> = std::result::Result<T, Box<dyn Error>>;
+#[derive(Error, Debug)]
+pub enum TermError {
+    #[error("polling failed: {0}")]
+    Poll(String),
+    #[error("failed to restore terminal: {0}")]
+    Restore(String),
+    #[error("failed to init terminal: {0}")]
+    Init(String),
+}
 
 /// Prepare the terminal by flushing the screen and hiding the cursor
-pub fn init_terminal() -> TerminalResult<Terminal<CrosstermBackend<io::Stdout>>> {
-    crossterm::execute!(io::stdout(), EnterAlternateScreen)?;
-    enable_raw_mode()?;
+pub fn init_terminal() -> Result<Terminal<CrosstermBackend<io::Stdout>>, TermError> {
+    crossterm::execute!(io::stdout(), EnterAlternateScreen)
+        .map_err(|e| TermError::Init(e.to_string()))?;
+    enable_raw_mode().map_err(|e| TermError::Init(e.to_string()))?;
 
     let backend = CrosstermBackend::new(io::stdout());
 
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
+    let mut terminal = Terminal::new(backend).map_err(|e| TermError::Init(e.to_string()))?;
+    terminal
+        .hide_cursor()
+        .map_err(|e| TermError::Init(e.to_string()))?;
 
     Ok(terminal)
 }
 
 /// Restore the terminal to its initial state
-pub fn restore_terminal() -> TerminalResult<()> {
-    disable_raw_mode()?;
-    crossterm::execute!(io::stdout(), LeaveAlternateScreen)?;
+pub fn restore_terminal() -> Result<(), TermError> {
+    disable_raw_mode().map_err(|e| TermError::Restore(e.to_string()))?;
+    crossterm::execute!(io::stdout(), LeaveAlternateScreen)
+        .map_err(|e| TermError::Restore(e.to_string()))?;
 
     Ok(())
 }
@@ -34,9 +46,15 @@ pub fn restore_terminal() -> TerminalResult<()> {
 /// Poll for crossterm events such as key events. This blocks the current
 /// thread for 1 second before timeout'ing, letting the application
 /// continue execution.
-pub fn poll_event() -> TerminalResult<Option<Event>> {
-    if event::poll(EVENT_TIMEOUT)? {
-        Ok(Some(event::read()?))
+pub fn poll_event() -> Result<Option<Event>, TermError> {
+    let outcome = match event::poll(EVENT_TIMEOUT) {
+        Ok(b) => b,
+        Err(e) => Err(TermError::Poll(e.to_string()))?,
+    };
+    if outcome {
+        Ok(Some(
+            event::read().map_err(|e| TermError::Poll(e.to_string()))?,
+        ))
     } else {
         Ok(None)
     }
