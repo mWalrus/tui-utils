6d0e95fe685c131f2aa7474725f62457436c2eb9
mWalrus <hellkvistoskar@protonmail.com>
2022-11-27 19:00:57 +0100

    chore: updating example with new features

--- examples/split_view.rs
+++ examples/split_view.rs
@@ -1,14 +1,14 @@
-use crossterm::event::{Event, KeyCode, KeyModifiers};
+use std::error::Error;
+
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 use tui::{style::Color, widgets::Paragraph};
 use tui_utils::{
     blocks,
-    component::Component,
+    component::{Component, Focus},
     keymap::{key_match, Keybind},
     split::{self, Ratio},
     term,
 };
-
-// FIXME: comment code better
 
 struct Split {
     text_left: String,
@@ -18,22 +18,30 @@ struct Split {
 
 impl Component for Split {
     fn draw<B: tui::backend::Backend>(&mut self, f: &mut tui::Frame<B>, _dim: bool) {
+        // define a new ratio of 50/50
         let ratio = Ratio::new(50, 50);
+        // create a vertical split using the frame size and ratio
         let chunks = split::v_split(f.size(), ratio);
+
+        // paragraphs as an example using default blocks
         let left_p =
             Paragraph::new(&*self.text_left).block(blocks::default_block("Left", Color::White));
         let right_p =
             Paragraph::new(&*self.text_right).block(blocks::default_block("Right", Color::White));
+
+        // render the widgets
         f.render_widget(left_p, chunks[0]);
         f.render_widget(right_p, chunks[1]);
     }
 
-    fn handle_input(&mut self, key: crossterm::event::KeyEvent) -> anyhow::Result<()> {
-        // find out a better way to handle quit
+    fn handle_input(&mut self, key: KeyEvent) -> Result<Focus, Box<dyn Error>> {
         if key_match(&key, &self.binds.quit) {
-            Err(anyhow::anyhow!("Exit application"))?
+            // unfocus the component if the quit key is pressed
+            Ok(Focus::Release)
+        } else {
+            // otherwise we do nothing
+            Ok(Focus::Keep)
         }
-        Ok(())
     }
 }
 
@@ -57,8 +65,10 @@ impl KeyBinds {
 }
 
 fn main() {
-    let mut terminal = term::init_terminal().unwrap();
+    // init the terminal
+    let mut terminal = term::init().unwrap();
 
+    // example application component
     let split = Split {
         text_left: String::from("left side"),
         text_right: String::from("right side"),
@@ -67,22 +77,31 @@ fn main() {
     let mut app = App { split };
 
     loop {
+        // draw the ui first
         terminal.draw(|f| app.split.draw(f, false)).unwrap();
 
+        // then handle input events
         match term::poll_event() {
-            Ok(Some(Event::Key(ev))) => {
-                match app.split.handle_input(ev) {
-                    Ok(()) => {}
-                    Err(_) => break, // break for convenience
+            Ok(Some(Event::Key(ev))) => match app.split.handle_input(ev) {
+                Ok(Focus::Keep) => {}
+                Ok(Focus::Release) => break, // exit on unfocus
+                Err(e) => {
+                    term::restore_with_err(e).unwrap();
+                    return;
                 }
-            }
-            Ok(None) | Ok(Some(_)) => {}
+            },
+            // other term events, we dont handle them in this example
+            Ok(Some(_)) => {}
+            // no events were found
+            Ok(None) => {}
+            // something went wrong
             Err(e) => {
-                eprintln!("{e}");
-                break;
+                term::restore_with_err(e.into()).unwrap();
+                return;
             }
         }
     }
 
-    term::restore_terminal().unwrap()
+    // always restore the terminal before exiting
+    term::restore().unwrap()
 }
