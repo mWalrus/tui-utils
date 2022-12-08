ada98d7dcd464fc1fac30539c18ef50bc000a85f
mWalrus <hellkvistoskar@protonmail.com>
2022-12-08 13:55:26 +0100

    feat(ratio): normalize ratio if sum exceeds 100
    Since the ratio should be a percentage ratio adding up to, at most, 100%
    we want to normalize all input that sums up above 100.
    
    This addition achieves that.

--- src/split.rs
+++ src/split.rs
@@ -19,7 +19,21 @@ impl Ratio {
     /// let ratio = Ratio::new(70, 30);
     /// ```
     pub fn new(first: u16, second: u16) -> Self {
-        Self(first, second)
+        // no need to normalize the ratio
+        if first + second <= 100 {
+            return Self(first, second);
+        }
+
+        // cast to f32
+        let first = first as f32;
+        let second = second as f32;
+
+        // normalize the ratio
+        let norm = 1.0 / (first + second);
+        let first = ((first * norm) * 100.0).ceil();
+        let second = ((second * norm) * 100.0).floor();
+
+        Self(first as u16, second as u16)
     }
 }
 
